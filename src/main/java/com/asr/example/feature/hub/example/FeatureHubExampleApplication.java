5fb81b2a78594b4ced1714799262b84bd05ce245
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2023-04-05 21:14:03 +0530

    Boiler plate setup

---
+++ src/main/java/com/asr/example/feature/hub/example/FeatureHubExampleApplication.java
@@ -0,0 +1,13 @@
+package com.asr.example.feature.hub.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureHubExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureHubExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asr/example/feature/hub/example/FeatureHubExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asr.example.feature.hub.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureHubExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
