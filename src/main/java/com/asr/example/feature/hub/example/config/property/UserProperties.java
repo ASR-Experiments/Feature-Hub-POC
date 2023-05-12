c98f900dc38ec6de7ff243b3db78dd58a12390dc
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2023-05-12 19:18:49 +0530

    Added logic to pre-populate the redis before application starts

--- src/main/java/com/asr/example/feature/hub/example/config/property/UserProperties.java
+++ src/main/java/com/asr/example/feature/hub/example/config/property/UserProperties.java
@@ -1,22 +1,35 @@
 package com.asr.example.feature.hub.example.config.property;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
-import javax.annotation.PostConstruct;
-import lombok.Getter;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserProperties {
 
-  Map<String, List<String>> agentsPhases;
+    Map<String, List<String>> agentsPhases;
 
-  @PostConstruct
-  void postConstruct() {
-    agentsPhases = Map.of(
-        "test2@yopmail.com", List.of("PHASE2"),
-        "test1@yopmail.com", List.of("PHASE1")
-    );
-  }
+    final RedisTemplate<String, String> redisTemplate;
+
+    @PostConstruct
+    void postConstruct() {
+        agentsPhases = Map.of(
+                "test2@yopmail.com", List.of("PHASE2"),
+                "test1@yopmail.com", List.of("PHASE1")
+        );
+
+        SetOperations<String, String> ops = redisTemplate.opsForSet();
+        agentsPhases.forEach((key, value) -> ops.add(key, value.toArray(String[]::new)));
+    }
 }
